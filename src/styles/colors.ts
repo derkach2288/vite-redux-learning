8744bb58997a5365cd8a58758c89bc3b527f5cea
derkach2288 <derkach2288@gmail.com>
2023-12-13 13:48:56 +0100

    initial commit

---
+++ src/styles/colors.ts
@@ -0,0 +1,13 @@
+interface Colors {
+  primary: string;
+  button: string;
+  white: string;
+  black: string;
+}
+
+export const colors: Colors = {
+  primary: "rgb(26, 35, 53)",
+  button: "#050599",
+  white: "#ffffff",
+  black: "#000000",
+};
