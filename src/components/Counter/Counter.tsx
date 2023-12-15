b428894fe6603ab7912b49930c578f48c43de3d5
derkach2288 <derkach2288@gmail.com>
2023-12-15 10:37:36 +0100

    update divide & multiply

--- src/components/Counter/Counter.tsx
+++ src/components/Counter/Counter.tsx
@@ -7,29 +7,30 @@ function Counter() {
   const [count, setCount] = useState<number>(0)
 
   const onPlus = () => {
-    setCount((prevValue) => prevValue + 1)
-    // setCount((prevValue) => parseFloat((prevValue + 1).toFixed(2)))
+    setCount((prevValue: number) => prevValue + 1)
+    // setCount((prevValue: number) => parseFloat((prevValue + 1).toFixed(2)))
   }
 
   const onMinus = () => {
-    setCount((prevValue) => prevValue - 1)
+    setCount((prevValue: number) => prevValue - 1)
   }
 
   const onMultiply = () => {
-    setCount((prevValue) => prevValue * 2)
+    setCount((prevValue: number) => prevValue * 2)
   }
 
   const onDivide = () => {
-    // setCount((prevValue) => Math.round((prevValue / 2) * 100) / 100 )
-    setCount((prevValue) => parseFloat((prevValue / 2).toFixed(2)))
+    // setCount((prevValue: number) => Math.round((prevValue / 2) * 100) / 100 )
+    // setCount((prevValue: number) => parseFloat((prevValue / 2).toFixed(2)))
+    setCount((prevValue: number) => prevValue / 2)
   }
 
   return (
     <CounterWrapper>
       <Button name="divide" onClick={onDivide} />
-      <Button name="-" onClick={onMinus} />
-      <Count>{count}</Count>
-      <Button name="+" onClick={onPlus} />
+      <Button name="minus" onClick={onMinus} />
+      <Count>{Number(count.toFixed(2))}</Count>
+      <Button name="plus" onClick={onPlus} />
       <Button name="multiply" onClick={onMultiply} />
     </CounterWrapper>
   )
