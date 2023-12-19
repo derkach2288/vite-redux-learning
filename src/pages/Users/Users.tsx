ecf334854908931bd4972119f21176df3f868560
derkach2288 <derkach2288@gmail.com>
2023-12-19 11:17:49 +0100

    add uuid

--- src/pages/Users/Users.tsx
+++ src/pages/Users/Users.tsx
@@ -1,4 +1,5 @@
 import { useSelector } from "react-redux"
+import {v4} from "uuid"
 import { usersState } from "store/redux/users/selectors"
 import {User} from "store/redux/users/type"
 
@@ -6,10 +7,11 @@ import { UsersPageWrapper, UserCard, ParagraphStylesBody } from "./styles"
 
 function Users() {
   const usersSt = useSelector(usersState)
+  console.log(v4());
   return (
     <UsersPageWrapper>
       {usersSt.users.map((user: User) => (
-        <UserCard>
+        <UserCard key={v4()}>
           <ParagraphStylesBody>{user.firstLastName}</ParagraphStylesBody>
           <ParagraphStylesBody>{user.age}</ParagraphStylesBody>
           <ParagraphStylesBody>{user.jobTitle}</ParagraphStylesBody>
