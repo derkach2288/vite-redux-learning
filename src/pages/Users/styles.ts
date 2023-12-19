d4e2f1b28ba6539d7e18a4f1b17fdc7e9b033837
derkach2288 <derkach2288@gmail.com>
2023-12-19 10:59:55 +0100

    add hw 32

--- src/pages/Users/styles.ts
+++ src/pages/Users/styles.ts
@@ -1,22 +1,25 @@
 import styled from "styled-components"
 
-import {colors} from 'styles/colors'
+import { colors } from "styles/colors"
 
 export const UsersPageWrapper = styled.div`
   display: flex;
   flex-wrap: wrap;
-  flex: 1;
+  align-items: stretch; // подстраивает карточки по высоте
+  height: fit-content;
+  /* flex: 1; */
   gap: 10px;
-  justify-content: center;
+  /* justify-content: center; */
 `
 
 export const UserCard = styled.div`
   display: flex;
   flex-direction: column;
   gap: 20px;
-  min-width: 200px;
+  /* min-width: 200px; */
+  width: 300px; // фиксированная ширина
   min-height: 150px;
-  height: fit-content;
+  /* height: fit-content; */ // это свойство не нужно т.к высота подстраивается от родительского свойства align-items: stretch;
   padding: 20px;
   border-radius: 8px;
   border: 1px solid black;
@@ -32,4 +35,18 @@ export const ParagraphStylesBody = styled.p`
   width: 100%;
   font-weight: bold;
   font-family: "YourFontFamily", sans-serif;
-  `;
+  overflow-wrap: break-word; // перенос текста
+
+`
+/*
+Как показать точеки, когда текст длинный
+1.Задаете главному контейнеру фиксированную ширину, например 300px
+2. Добавляете св-ва тексту: width: 100%;
+  text-overflow: ellipsis; // показывает точки 
+  overflow: hidden; // скрывает текст что не влез
+  */
+
+  /*
+Если вы хотите перенносить текст на другие строки, то добавьте вашему тегу с текстом след св-во:
+overflow-wrap: break-word;
+  */
